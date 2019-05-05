3b2e15d2c0b450d6cbc7dfdf26c0b3caa075feb6
antipodal <50287153+antipodal@users.noreply.github.com>
2019-05-05 16:51:10 +0900

    Update C.c

--- C.c
+++ C.c
@@ -7,10 +7,12 @@ main()
 	int a = 1, b = 2;
 	char* cp = &a;
 	int* ip = &b;
-
+	/* *<-값을 나타 낸다는 의미, &<-주소값 반환 */
+	
 	printf("%x, %x, %x, %x\n", *cp, &cp, cp, *&cp);
+	/* *cp == a, &cp == cp의 주소 값, cp == &a, *&cp == cp(의 주소 값)의 값(==&a)*/
+	
 	printf("%x, %x, %x, %x\n", *ip, &ip, ip, *&ip);
-
 	return 0;
 
 	}
